077930cce10591b3d4c32a766ce254eb643f1064
vasco <jhades.dev@gmail.com>
2026-08-31 16:21:56 +0200

    Angular For Beginners (Signals Edition) recordings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Toolbar } from './toolbar/toolbar';
 import { Courses } from './courses/courses';
+import {CourseCard} from './course-card/course-card';
 
 @Component({
   selector: 'root',
-  imports: [Toolbar, Courses],
+  imports: [Toolbar, Courses,CourseCard],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
