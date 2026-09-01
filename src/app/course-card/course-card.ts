8c7d147c820d315fc262d3120f4650ad76520cba
vasco <jhades.dev@gmail.com>
2026-09-01 14:19:41 +0200

    Angular For Beginners (Signals Edition) recordings

--- src/app/course-card/course-card.ts
+++ src/app/course-card/course-card.ts
@@ -10,4 +10,16 @@ import { Course } from '../model/course';
 })
 export class CourseCard {
 
+  course: Course = {
+    id: 23,
+    title: 'Angular For Beginners (Signals Edition) v3',
+    description:
+      'Learn Angular from scratch using the new Signals API — build real apps with the modern Angular paradigm v3',
+    iconUrl:
+      'https://d3vigmphadbn9b.cloudfront.net/course-images/large-images/angular-for-beginners.jpg',
+    category: 'BEGINNER v3',
+    seqNo: 0,
+    price: 0,
+  };
+
 }
