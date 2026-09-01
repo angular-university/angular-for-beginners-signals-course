ce488de8a98f7a3461a2f2b5ca50cf7e2fd8ca0b
vasco <jhades.dev@gmail.com>
2026-09-01 16:01:48 +0200

    Angular For Beginners (Signals Edition) recordings

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Toolbar } from './toolbar/toolbar';
 import { Courses } from './courses/courses';
 import {CourseCard} from './course-card/course-card';
+import {Course} from './model/course';
 
 @Component({
   selector: 'root',
@@ -10,5 +11,17 @@ import {CourseCard} from './course-card/course-card';
   styleUrl: './app.scss',
 })
 export class App {
+
+  courseData: Course = {
+    id: 23,
+    title: 'Angular For Beginners (Signals Edition)',
+    description:
+      'Learn Angular from scratch using the new Signals API — build real apps with the modern Angular paradigm',
+    iconUrl:
+      'https://d3vigmphadbn9b.cloudfront.net/course-images/large-images/angular-for-beginners.jpg',
+    category: 'BEGINNER',
+    seqNo: 0,
+    price: 0,
+  };
 
 }
--- src/app/course-card/course-card.ts
+++ src/app/course-card/course-card.ts
@@ -10,25 +10,15 @@ import { Course } from '../model/course';
 })
 export class CourseCard {
 
-  course: Course = {
-    id: 23,
-    title: 'Angular For Beginners (Signals Edition)',
-    description:
-      'Learn Angular from scratch using the new Signals API — build real apps with the modern Angular paradigm',
-    iconUrl:
-      'https://d3vigmphadbn9b.cloudfront.net/course-images/large-images/angular-for-beginners.jpg',
-    category: 'BEGINNER',
-    seqNo: 0,
-    price: 0,
-  };
+  course = input.required<Course>();
 
   onCardClick() {
-    console.log('card clicked:', this.course.title);
+    console.log('card clicked:', this.course().title);
   }
 
   onEditClick(event: MouseEvent) {
     console.log('edit clicked:', event.target);
-    this.course.title += ' v2';
+    // this.course.title += ' v2';
     event.stopPropagation();
   }
 
